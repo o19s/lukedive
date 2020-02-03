da523b8eda89363cd54a092afa558978981f6992
Daniel Worley <danworley@gmail.com>
2020-02-03 00:14:51 -0500

    Hook graph up to luke data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,14 +21,15 @@ export class AppComponent {
   // Graph data
   private width: number;
   private height: number;
-  private margin = { top: 20, right: 20, bottom:30, left: 40 };
+  private margin = { top: 20, right: 20, bottom:150, left: 40 };
 
   private svg: any;
   private g: any;
   private x: any;
   private y: any;
 
-  increasingData = [ 1, 2, 4, 8, 16, 32 ];
+  private fieldData = null;
+
 
   solrUrl = 'http://quepid-solr.dev.o19s.com:8985/solr/tmdb/admin/luke?fl=*';
   title = 'lukedive';
@@ -41,15 +42,19 @@ export class AppComponent {
   ) { }
 
   ngOnInit() {
-    this.initSvg();
-    this.initAxis();
-
     // Snag the luke data from TMDB
     // TODO: Hook up a textbox and form so the user can specify any URL
     this.solr.fetchLuke(this.solrUrl)
       .subscribe(
         (data) => {
             this.lukeData = data;
+            this.fieldData = [];
+
+            // Organize field term counts into a structure easier to throw into d3
+            for ( let [key, value] of Object.entries(this.lukeData.fields)) {
+                this.fieldData.push({field: key, count: value.distinct});
+            }
+
             this.refresh();
         }
       );
@@ -57,6 +62,8 @@ export class AppComponent {
 
 
   refresh() {
+    this.initSvg();
+    this.initAxis();
     this.drawAxis();
     this.drawBars();
   }
@@ -73,36 +80,41 @@ export class AppComponent {
   private initAxis() {
     this.x = d3Scale.scaleBand().rangeRound([0, this.width]).padding(0.1);
     this.y = d3Scale.scaleLinear().rangeRound([this.height, 0]);
-    this.x.domain(STATISTICS.map((d) => d.letter));
-    this.y.domain([0, d3Array.max(STATISTICS, (d) => d.frequency)]);
+    this.x.domain(this.fieldData.map((d) => d.field));
+    this.y.domain([0, d3Array.max(this.fieldData, (d) => d.count)]);
   }
 
   private drawAxis() {
     this.g.append('g')
       .attr('class', 'axis x-axis')
       .attr('transform', 'translate(0,' + this.height + ')')
-      .call(d3Axis.axisBottom(this.x));
+      .call(d3Axis.axisBottom(this.x))
+      .selectAll('text')
+        .style('text-anchor', 'end')
+        .attr('dx', '-.8em')
+        .attr('dy', '.15em')
+        .attr('transform', 'rotate(-65)');
 
     this.g.append('g')
       .attr('class', 'axis y-axis')
-      .call(d3Axis.axisLeft(this.y).ticks(10, '%'))
+      .call(d3Axis.axisLeft(this.y).ticks(10))
       .append('text')
       .attr('class', 'axis-title')
       .attr('transform', 'rotate(-90)')
       .attr('y', 6)
       .attr('dy', '0.71em')
       .attr('text-anchor', 'end')
-      .text('Frequency');
+      .text('Term Count');
   }
 
   private drawBars() {
     this.g.selectAll('.bar')
-      .data(STATISTICS)
+      .data(this.fieldData)
       .enter().append('rect')
       .attr('class', 'bar')
-      .attr('x', (d) => this.x(d.letter))
-      .attr('y', (d) => this.y(d.frequency))
+      .attr('x', (d) => this.x(d.field))
+      .attr('y', (d) => this.y(d.count))
       .attr('width', this.x.bandwidth())
-      .attr('height', (d) => this.height - this.y(d.frequency));
+      .attr('height', (d) => this.height - this.y(d.count));
   }
 }
